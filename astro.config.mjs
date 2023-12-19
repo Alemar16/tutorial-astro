095c19b73e0979492a514d8dce48748e2e5504c3
Armando Martinez <alemar.martinez16@gmail.com>
2023-12-19 12:50:12 -0300

    Construye tu primera isla de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://alemar16-tutorial-astro.netlify.app/"
+  site: "https://alemar16-tutorial-astro.netlify.app/",
+  integrations: [preact()]
 });
